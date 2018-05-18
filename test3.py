c71ec64c62733f234d02b6dafb9892b7b1e278df
Janis Rumnieks <janis.rumnieks@gmail.com>
2018-05-18 11:32:30 +0100

    test git updates

--- test3.py
+++ test3.py
@@ -34,7 +34,7 @@ resss = infa_connection.create_repository_folder("JANIS_from_PMREP_test2")
 
 ffff = infa_connection.export_repository_folder("BIDW_CUSTOM_SILOS","Auto_export_BIDW_CUSTOM_SILOS.xml")
 
-
+#
 
 
 
